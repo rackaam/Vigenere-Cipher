c11ab1bd27793214798e8603bc3f9b5e091d4309
Antoine Foucault <15008135@e008m06.istic.univ-rennes1.fr>
2014-10-15 10:05:31 +0200

    Calcul de la longueur de la clé OK

--- casseur.c
+++ casseur.c
@@ -5,71 +5,152 @@
 #include "my_string.h"
 
 #define BAD_ARGS -2
+#define IC_SEUIL 0.06
+#define MAX_TAILLE_CLE 10
 
-void calculer_occ(string str, int occ[]);
-double calculer_ic(string str, int distance);
+void calculer_occ(string *str, double occ[]);
+double calculer_ic(string *str);
 string* extraire(string source, int distance);
+int longueur_cle(string *s);
+int icOK(double ic);
+string* getSousChaine(string *base, int decalage, int espacement);
+
+string* getSousChaine(string *base, int decalage, int espacement){
+
+	if(base == NULL){
+	
+		return NULL;
+	}
+	
+	string *res = NULL;
+	int i, j, taille_alloc = (base->length)/espacement;
+	
+	if((res = (string *)malloc(sizeof(string))) == NULL){
+	
+		perror("Erreur dans l'allocation d'une structure string");
+	}
+	
+	if((res->content = (char *)malloc(taille_alloc+1 * sizeof(char))) == NULL){
+	
+		perror("Erreur dans l'allocation d'une chaîne pour la structure string");
+	}
+	
+	for(i=0; (i*espacement + decalage) < base->length; i++){
+	
+		res->content[i] = base->content[i*espacement + decalage];
+	}
+	
+	res->content[taille_alloc] = '\0';
+	res->length = taille_alloc;
+	
+	return res;
+}
+
+int icOK(double ic){
+
+	return (ic > IC_SEUIL);
+}
+
+int longueur_cle(string *s){
+
+	int resultat = -1, i = 1, j;
+	double ic, moyenne_ic = 0.0;
+	string *sous_chaine = NULL;
+	
+	do{
+	
+		i++;
+		ic = 0.0;
+		
+		for(j=0; j<i; j++){
+			
+			if((sous_chaine = getSousChaine(s, j, i)) == NULL){
+			
+				fprintf(stderr, "Erreur dans la création d'une sous_chaine");
+				exit(1);
+			}
+			
+			//printf("%s\n", sous_chaine->content);
+			ic += calculer_ic(sous_chaine);
+			
+			free(sous_chaine->content);
+			free(sous_chaine);
+		}
+		
+		moyenne_ic = ic/j;
+		printf("Moyenne ic : %lf\n", moyenne_ic);
+
+	}while(!icOK(moyenne_ic) && i < MAX_TAILLE_CLE);
+	
+	if(icOK(moyenne_ic)){
+	
+		return i;
+	}
+	
+	return resultat;
+}
 
 int main(int argc, char *argv[]){
 	
 	char* file_in;
-	double ic;
+	int l = 0;
 	
 	if(argc != 2)
 	{
-		fprintf(stderr, "Erreur BAD_ARGS\n"); 
+		fprintf(stderr, "USAGE %s [ENCRYPTED FILE]\n", argv[0]); 
 		exit(BAD_ARGS);
 	}
 	
 	file_in = argv[1];
-	
 	string str = readstring(file_in);
 	
-	int i;
-	for(i = 1; i < 50; i++)
-	{
-		ic = calculer_ic(str, i);
-		printf("IC %d => %lf\n", i, ic);
-		
-	} 
+	if((l = longueur_cle(&str)) != -1){
 	
-	return 0;
+		printf("La longueur de la clé est %i\n", l);
+	}
+	
+	return EXIT_SUCCESS;
 }
 
-double calculer_ic(string str, int distance){
+double calculer_ic(string *str){
 
-	int occ[128];
-	string* sousChaine = extraire(str, distance);
-	calculer_occ(*sousChaine, occ);
-	double longueur = (double)sousChaine->length;
-	double ic = 0;
-	float tab_freq[128];
+	double occ[128];
+	double longueur = (double)str->length;
+	double ic = 0.0;
+	
+	//float tab_freq[128];
 	int i;
+	
+	calculer_occ(str, occ);
 	
 	for(i=0; i<128; i++){
 	
-		tab_freq[i] = (occ[i]/longueur);
-		ic += tab_freq[i] * (tab_freq[i] - 1);
+		ic += occ[i] * (occ[i] - 1);
 	}
 	
-	printf("IC => %lf et %lf\n", ic, (longueur * (longueur-1))); 
+	//printf("IC => %lf et %lf\n", ic, (longueur * (longueur-1))); 
+	//printf("IC unitaire avant : %lf\n", ic);
 	ic = ic / (longueur * (longueur-1));
 	
-	free(sousChaine);
+	//printf("IC unitaire : %lf\n", ic);
+	
 	return ic;
 }
 
-void calculer_occ(string str, int occ[])
+void calculer_occ(string *str, double occ[])
 {
 	int i;
+	
 	for(i = 0; i < 128; i++)
-		occ[i] = 0;
-	for(i = 0; i < str.length; i++)
+		occ[i] = 0.0;
+		
+	for(i = 0; i < str->length; i++)
 	{
-		occ[(int)str.content[i]] = occ[(int)str.content[i]] + 1; 
+		occ[(int)str->content[i]] = occ[(int)str->content[i]] + 1; 
 	}
+	
 	/*for(i = 0; i < 128; i++)
-		printf("\n%d=>%d",i,occ[i]);*/
+		printf("\n%d=>%lf",i,occ[i]);*/
 }
 
 string* extraire(string source, int distance)
